6b24544815192078fe231214605a2aac281ea528
Zepeng Chu <zepeng.chu@enpc.fr>
2025-12-31 12:04:18 +0100

    add new file

---
+++ example_predefined_algos.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from typing import Dict, Any
+
+import numpy as np
+
+from timeeval import TimeEval, DatasetManager, Algorithm, TrainingType, InputDimensionality
+from timeeval.adapters import FunctionAdapter
+from timeeval.algorithms import subsequence_if,copod
+from timeeval.params import FixedParameters
+
+# Load dataset metadata
+dm = DatasetManager(Path("data"), create_if_missing=False)
+
+# Select datasets and algorithms
+datasets = dm.select()
+datasets = datasets[-1:]
+# Add algorithms to evaluate...
+algorithms = [
+    copod(),
+]
+timeeval = TimeEval(dm, datasets, algorithms)
+
+# execute evaluation
+timeeval.run()
+# retrieve results
+print(timeeval.get_results())
